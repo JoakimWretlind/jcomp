4536ec4512977560c94dcfa136bbc76f1a049756
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-23 11:39:09 +0100

    Making the context avaliable to the whole application

--- src/App.tsx
+++ src/App.tsx
@@ -2,17 +2,20 @@ import { GlobalStyle, searchTheme } from "./styles/globalStyle";
 import { ThemeProvider } from "styled-components";
 import { Routes, Route } from "react-router-dom";
 import { HomePage, DetailsPage, ErrorsPage } from "./pages";
+import ProductsProvider from "./components/context/ProductsProvider";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <ThemeProvider theme={searchTheme}>
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/products/:productID" element={<DetailsPage />} />
-          <Route path="*" element={<ErrorsPage />} />
-        </Routes>
+        <ProductsProvider>
+          <Routes location={location} key={location.pathname}>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/products/:productID" element={<DetailsPage />} />
+            <Route path="*" element={<ErrorsPage />} />
+          </Routes>
+        </ProductsProvider>
       </ThemeProvider>
     </>
   );
