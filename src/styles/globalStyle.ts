82a22eb99e2518735822557d1810ffba27d3a061
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-23 12:01:56 +0100

    Added shadow to the theme

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -7,6 +7,9 @@ export const searchTheme = {
   darkBlack: "hsl(0, 0%, 10%)",
   midBlack: "hsl(0, 0%, 15%)",
 
+  cardShadow: "0 0.2rem 0.4rem rgba(2, 2, 2, 0.2)",
+  cardShadowHover: "0 0.3rem 0.6rem rgba(2, 2, 2, 0.2)",
+
   xsScreen: "380px",
   smallScreen: "620px",
   tabletScreen: "768px",
