3e3ffa44d5378ec4833535c4ce4693abef703f74
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-24 11:41:17 +0100

    Edited some global themes

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -4,14 +4,14 @@ export const searchTheme = {
   lightBlue: "hsl(188, 94%, 89%)",
   blue: "hsl(188, 74%, 39%)",
   brightWhite: "hsl(0, 0%, 98%)",
-  midtWhite: "hsl(240, 5%, 96%)",
+  lightWhite: "hsl(240, 5%, 96%)",
+  midWhite: "hsl(240, 5%, 91%)",
   gray: "hsl(0, 0%, 70%)",
   darkBlack: "hsl(0, 0%, 10%)",
   midBlack: "hsl(0, 0%, 15%)",
   red: "hsl(0, 88%, 37%)",
 
-  buttonShadow: "0 0.2rem 0.4rem rgba(2, 2, 2, 0.3)",
-  buttonShadowHover: "0 0 0.2rem rgba(2, 2, 2, 0.3)",
+  buttonShadow: "0 0.2rem 0.4rem rgba(13, 65, 73, 0.3)",
   cardShadow: "0 0.2rem 0.4rem rgba(2, 2, 2, 0.2)",
   cardShadowHover: "0 0.3rem 0.6rem rgba(2, 2, 2, 0.2)",
 
