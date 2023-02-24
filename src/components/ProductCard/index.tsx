8a87ecfe0bf9d019e278074fcb5b140bb272ec85
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-24 21:11:25 +0100

    refactor some code

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,22 +1,22 @@
 import IProducts from "../interfaces/IProducts";
-import { Card, CardContent } from "./style";
+import { Card } from "./style";
 
 export const ProductCard = (product: IProducts) => {
   const { displayimage, brand, category, model, displayDetail, price } =
     product;
   return (
     <Card>
-      <CardContent>
+      <div className="left">
         <img src={displayimage} alt="img" />
-        <div>
-          <h5>{brand}</h5>
-          <h6>{category}</h6>
-          <hr />
-          <p className="model">{model}</p>
-          <p>{displayDetail}</p>
-          <p>{price}</p>
-        </div>
-      </CardContent>
+      </div>
+      <div className="right">
+        <h5>{brand}</h5>
+        <h6>{category}</h6>
+        <hr />
+        <p className="model">{model}</p>
+        <p className="details">{displayDetail}</p>
+        <p className="price">Price: ${price}</p>
+      </div>
     </Card>
   );
 };
--- src/components/ProductCard/style.ts
+++ src/components/ProductCard/style.ts
@@ -1,68 +1,100 @@
 import styled from "styled-components";
 
 export const Card = styled.div`
-  height: max-content;
-  width: 100%;
+  width: 24rem;
   border-radius: 1rem;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
   box-shadow: ${(props) => props.theme.cardShadow};
   background-color: ${(props) => props.theme.brightWhite};
-  aspect-ratio: 16/9;
+  padding: 2rem 1rem;
+  margin: 1rem;
+  color: ${(props) => props.theme.midBlack};
   transition: 0.3s ease;
+  @media (min-width: ${(props) => props.theme.xsScreen}) {
+    width: 34rem;
+    padding: 1rem;
+    aspect-ratio: 16/8;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: row;
+  }
   &:hover {
     transform: translateY(-0.1rem);
     box-shadow: ${(props) => props.theme.cardShadowHover};
     filter: contrast(110%);
   }
-`;
-
-export const CardContent = styled.div`
-  height: 100%;
-  width: 100%;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 1rem;
-  color: ${(props) => props.theme.midBlack};
-  img {
-    width: clamp(10rem, 50%, 16rem);
+  .left {
+    width: 50%;
     height: 100%;
-    max-height: 12rem;
-    margin-bottom: 1rem;
-    object-fit: contain;
-    margin-right: 0.5rem;
-  }
-  h5 {
-    text-transform: capitalize;
-    font-size: 1.8rem;
-    line-height: 0.8;
-  }
-  h6 {
-    margin-top: 0.2rem;
-    font-size: 1.2rem;
-    line-height: 1.4;
-    font-weight: 500;
-    letter-spacing: 0.2rem;
-  }
-  p {
-    font-size: 1.2rem;
-    text-transform: capitalize;
-    &.model {
-      font-weight: 700;
-      margin-bottom: 0.4rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-right: 1rem;
+    img {
+      width: 100%;
+      height: 100%;
+      max-height: 12rem;
+      margin-bottom: 2rem;
+      object-fit: contain;
+      margin-right: 0.5rem;
+      @media (min-width: ${(props) => props.theme.xsScreen}) {
+        margin-bottom: 0;
+      }
     }
   }
-  hr {
-    border: none;
-    background-color: ${(props) => props.theme.gray};
-    height: 0.1rem;
-    margin: 0.1rem 0 0.2rem;
-  }
-  div {
+  .right {
+    height: 100%;
     width: 100%;
-    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
     @media (min-width: ${(props) => props.theme.xsScreen}) {
-      width: 14rem;
-      margin-left: 1rem;
+      width: 50%;
+    }
+    h5 {
+      text-transform: capitalize;
+      font-size: 1.8rem;
+      line-height: 0.8;
+      text-align: center;
+    }
+    h6 {
+      margin-top: 0.2rem;
+      font-size: 1.2rem;
+      line-height: 1.4;
+      font-weight: 500;
+      letter-spacing: 0.2rem;
+      text-align: center;
+    }
+    p {
+      font-size: 1.2rem;
+      text-transform: capitalize;
+      &.model {
+        font-weight: 700;
+        margin-bottom: 0.4rem;
+        text-align: center;
+      }
+      &.details {
+        text-align: center;
+      }
+      &.price {
+        margin-top: 1rem;
+        font-size: 1.4rem;
+        font-weight: 500;
+        text-align: center;
+        @media (min-width: ${(props) => props.theme.xsScreen}) {
+          text-align: left;
+        }
+      }
+    }
+    hr {
+      border: none;
+      background-color: ${(props) => props.theme.lightGray};
+      height: 0.1rem;
+      width: 100%;
+      margin: 0.1rem 0 0.5rem;
     }
   }
 `;
