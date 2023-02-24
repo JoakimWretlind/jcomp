f55f8ac4ba7cf4481ebcc7caced87ba01624e34f
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-24 11:41:30 +0100

    removed unused code

--- src/pages/HomePage/style.ts
+++ src/pages/HomePage/style.ts
@@ -1,31 +1,12 @@
 import styled from "styled-components";
 
-// export const HomePageContainer = styled.div`
-//   height: 100%;
-//   min-height: 100vh;
-//   width: 100%;
-//   display: flex;
-//   flex-direction: column;
-//   justify-content: flex-start;
-//   align-items: center;
-//   background-color: ${(props) => props.theme.midtWhite};
-//   h1 {
-//     margin: 3rem 0 1rem;
-//     font-size: 3rem;
-//     text-transform: capitalize;
-//     font-weight: 300;
-//     letter-spacing: 0.2rem;
-//     font-family: "Roboto", sans-serif;
-//   }
-// `;
-
 export const HomePageContainer = styled.div`
   height: 100%;
   min-height: 100vh;
   width: 100%;
   display: grid;
   grid-template-rows: 10vh minmax(76vh, 1fr) 14vh;
-  background-color: ${(props) => props.theme.midtWhite};
+  background-color: ${(props) => props.theme.midWhite};
 `;
 
 export const H1 = styled.h1`
