4184d1e03f7a67dbcee785eb67bdf3a679d09684
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-25 12:15:48 +0100

    testing deployment error

--- src/components/context/ProductsProvider.tsx
+++ src/components/context/ProductsProvider.tsx
@@ -24,6 +24,7 @@ function useProductProvider() {
 
   // Fetch data
   useEffect(() => {
+    console.log("fetching");
     const fetchData = async () => {
       try {
         const res = await fetch("../../../api/products.json");
