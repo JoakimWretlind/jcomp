6721e038bca4314ae6ee231d0d29a86c1ed40670
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-26 18:39:46 +0100

    for layout change

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -1,22 +1,22 @@
 import IProducts from "../interfaces/IProducts";
-import { Card } from "./style";
+import { Card, Left, Right } from "./style";
 
 export const ProductCard = (product: IProducts) => {
   const { displayimage, brand, category, model, displayDetail, price } =
     product;
   return (
     <Card>
-      <div className="left">
+      <Left>
         <img src={displayimage} alt="img" />
-      </div>
-      <div className="right">
+      </Left>
+      <Right>
         <h5>{brand}</h5>
         <h6>{category}</h6>
         <hr />
         <p className="model">{model}</p>
         <p className="details">{displayDetail}</p>
         <p className="price">Price: ${price}</p>
-      </div>
+      </Right>
     </Card>
   );
 };
