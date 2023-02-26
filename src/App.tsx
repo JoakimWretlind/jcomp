1c6197187e177bd4cbe37f11ec3e4ef6a100c5a9
JoakimWretlind <joakim.wretlind@gmail.com>
2023-02-26 18:40:47 +0100

    added preloader and logo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import ProductsProvider from "./components/context/ProductsProvider";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { HomePage, DetailsPage, ErrorsPage } from "./pages";
+import { Preloader } from "./components/preloader";
+import { Logo } from "./components/Logo";
 
 function App() {
   const location = useLocation();
@@ -12,6 +14,8 @@ function App() {
     <>
       <GlobalStyle />
       <ThemeProvider theme={searchTheme}>
+        <Preloader />
+        <Logo />
         <ProductsProvider>
           <AnimatePresence mode="wait">
             <Routes location={location} key={location.pathname}>
