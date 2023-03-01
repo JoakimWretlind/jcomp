3f5fa041c2fedb2867123f425da0ad9bc9dd40ef
JoakimWretlind <joakim.wretlind@gmail.com>
2023-03-01 16:55:12 +0100

    implemented hook for animation-duration

--- src/pages/DetailsPage/index.tsx
+++ src/pages/DetailsPage/index.tsx
@@ -12,26 +12,12 @@ import {
 } from "./style";
 import { AiOutlineArrowLeft } from "react-icons/ai";
 import { motion } from "framer-motion";
-import { useState, useEffect } from "react";
+import { useTiming } from "../../components/hooks/useTiming";
 
 const DetailsPage = () => {
   let { productID } = useParams();
   const { products } = useProductContext();
-
-  const [timing, setTiming] = useState<number>(0.3);
-  const screenWidth = window.innerWidth;
-
-  useEffect(() => {
-    if (screenWidth < 465) {
-      setTiming(0.25);
-    } else if (screenWidth >= 465 && screenWidth < 768) {
-      setTiming(0.35);
-    } else if (screenWidth >= 768 && screenWidth < 2000) {
-      setTiming(0.45);
-    } else {
-      setTiming(0.55);
-    }
-  });
+  const timing = useTiming();
 
   try {
     const product: IProducts = products?.find(
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,24 +1,11 @@
 import { motion } from "framer-motion";
-import { useEffect, useState } from "react";
 import { AllProducts } from "../../components/AllProducts";
 import { Footer } from "../../components/Footer";
+import { useTiming } from "../../components/hooks/useTiming";
 import { H1, HomePageContainer } from "./style";
 
 const HomePage = () => {
-  const [timing, setTiming] = useState<number>(0.3);
-  const screenWidth = window.innerWidth;
-
-  useEffect(() => {
-    if (screenWidth < 465) {
-      setTiming(0.25);
-    } else if (screenWidth >= 465 && screenWidth < 768) {
-      setTiming(0.35);
-    } else if (screenWidth >= 768 && screenWidth < 2000) {
-      setTiming(0.45);
-    } else {
-      setTiming(0.55);
-    }
-  });
+  const timing = useTiming();
 
   return (
     <HomePageContainer
