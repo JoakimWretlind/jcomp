9f8ae811fb7d809923c5a2a77880c856b002ccc7
JoakimWretlind <joakim.wretlind@gmail.com>
2023-03-02 11:03:41 +0100

    tweaking animation douration

--- src/pages/DetailsPage/index.tsx
+++ src/pages/DetailsPage/index.tsx
@@ -44,6 +44,7 @@ const DetailsPage = () => {
         as={motion.div}
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
+        transition={{ duration: 0.35 }}
         exit={{ opacity: 0 }}
       >
         <h2>{`${brand} ${model}`}</h2>
