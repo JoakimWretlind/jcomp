7badfed692bd56b20d0d814e04ddf37cde092ab0
JoakimWretlind <joakim.wretlind@gmail.com>
2023-03-05 18:32:02 +0100

    switching from BrowserRouter to HashRouter due to routing issues with netlify

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { ScrollToTop } from "./components/hooks/ScrollToTop";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
+    <HashRouter>
       <ScrollToTop />
       <App />
-    </Router>
+    </HashRouter>
   </React.StrictMode>
 );
