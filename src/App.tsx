e73293648e17b75b3827b46f50d06fa9ea4fdb1d
JoakimWretlind <joakim.wretlind@gmail.com>
2023-03-10 17:31:25 +0100

    removed lazy loading

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,9 @@ import { ThemeProvider } from "styled-components";
 import ProductsProvider from "./components/context/ProductsProvider";
 import { AnimatePresence } from "framer-motion";
 import { Routes, Route, useLocation } from "react-router-dom";
-import { ErrorsPage } from "./pages";
+import { DetailsPage, ErrorsPage, HomePage } from "./pages";
 import { Preloader } from "./components/preloader";
 import { Logo } from "./components/Logo";
-import { lazy, LazyExoticComponent, ComponentType } from "react";
-export const HomePage: LazyExoticComponent<ComponentType<any>> = lazy(
-  () => import("./pages/HomePage")
-);
-export const DetailsPage: LazyExoticComponent<ComponentType<any>> = lazy(
-  () => import("./pages/DetailsPage")
-);
 
 function App() {
   const location = useLocation();
