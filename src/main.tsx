83af17021346cb66f8fda5c8d9eb782f4f59449b
JoakimWretlind <joakim.wretlind@gmail.com>
2023-03-17 11:34:21 +0100

    put scrolltotop hook back on

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import { ScrollToTop } from "./components/hooks/ScrollToTop";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <HashRouter>
-      {/* <ScrollToTop /> */}
+      <ScrollToTop />
       <App />
     </HashRouter>
   </React.StrictMode>
